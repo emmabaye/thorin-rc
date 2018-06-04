60aa7af8a8d34d155b1aec174a50058a37567b96
chika onuchukwu <chuzksy-codeactive>
2018-06-04 17:27:28 +0100

    feature(customize): create theme - create a custom theme in navbar.less - import the navbar.less file and register it - create a inline script to customize the email templates [Finishes #157906013]

---
+++ imports/plugins/custom/thorinrc-theme/register.js
@@ -0,0 +1,10 @@
+import { Reaction } from "/server/api";
+
+Reaction.registerPackage({
+  label: "My Theme",
+  name: "thorinrc-theme",
+  autoEnable: true,
+  settings: {},
+  registry: [],
+  icon: "fa fa-bars"
+});
