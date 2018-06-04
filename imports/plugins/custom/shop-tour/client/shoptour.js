fdfb54158ed1acadc7746ad5c23177ba758284ad
Olusola Oseni <kenolusola@gmail.com>
2018-06-04 13:57:01 +0100

    feat(onboarding): implement onboarding feature - ensure first time customers are onboarded to the app by default - ensure customers can take a tour of the shop at anytime

---
+++ imports/plugins/custom/shop-tour/client/shoptour.js
@@ -0,0 +1,23 @@
+import introJs from "intro.js";
+import onboardingSteps from "./customerOnboardingSteps";
+
+const intro = introJs();
+const shopTour = () => {
+  const steps = onboardingSteps;
+  intro.setOptions({
+    overlayOpacity: 0.9,
+    showProgress: true,
+    exitOnEsc: true,
+    showStepNumbers: true,
+    stepNumberPosition: "top-left",
+    showBullets: true,
+    disableInteraction: false,
+    skipLabel: "Exit",
+    scrollToElement: true,
+    steps
+  });
+  intro.start();
+};
+
+
+export default shopTour;
--- imports/plugins/included/default-theme/client/index.js
+++ imports/plugins/included/default-theme/client/index.js
@@ -3,3 +3,5 @@ import "./favicons";
 
 // Scripts
 import "bootstrap/dist/js/npm.js";
+import "node_modules/intro.js/introjs.css";
+import "node_modules/intro.js/themes/introjs-modern.css";
